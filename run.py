7165fcbc98a2c168322c3027ab4cb1b4e9c4ba17
DaveTrev <trevaskisdavid@gmail.com>
2023-04-13 05:08:03 +0000

    open weather data status - working

--- run.py
+++ run.py
@@ -18,3 +18,9 @@ print(welcome)
 
 user_input = input("Enter your city: ")
 print(user_input)
+
+
+open_weather_data = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + \
+f"{user_input}&units=metric&APPID={config['API']['key']}")
+
+print(open_weather_data.status_code)
