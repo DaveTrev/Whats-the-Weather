45aafb92ad27a0c16ad63fe3e2eeb298c7ee7697
DaveTrev <trevaskisdavid@gmail.com>
2023-04-13 04:58:58 +0000

    inital commit, imported configparser for api key

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import configparser
+import requests
+import pyfiglet
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
