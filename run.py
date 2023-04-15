70ce6c285ba763cf4c8cd145c11a29bf603ac7d1
DaveTrev <trevaskisdavid@gmail.com>
2023-04-15 20:29:54 +0000

    menu layout in place but not working, need to add forecast options and api call

--- run.py
+++ run.py
@@ -10,13 +10,9 @@ give option to repeat program or quit
 readme
 heroku deploy
 
-
-while True:
-
-  print("1) Current Weather")
-  print("2) Forcast") api.openweathermap.org/data/2.5/forecast?q= +
+ api.openweathermap.org/data/2.5/forecast?q= +
   {city name}&appid={API key}
-  print("3) Quit")
+  
 
 
 """
@@ -24,34 +20,74 @@ while True:
 import configparser
 import requests
 import pyfiglet
-import colorama
-from colorama import Fore, Style
+from colorama import Fore
 
 # Use configparser to read config.ini file
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
 
 # pyfgilet used to style app greeting
 welcome = pyfiglet.figlet_format("Whats the weather?")
 print(Fore.GREEN + welcome)
+while True:
+
+    print("1) Current Weather")
+    print("2) Forcast")
+    print("3) Quit")
+
+    choice = input("Enter Choice: ")
+    # using strip method to removing leading trailing white space
+    choice = choice.strip()
 
 # Prompt user to enter a city
-user_input = input("Enter your city: ")
-print(user_input)
+if choice == "1":
+    user_input = input("Enter your city: ")
+    print(user_input)
 
-# Request data from openweather.com
-open_weather_data = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" +
-                                 f"{user_input}&units=metric&APPID=" +
-                                 f"{config['API']['key']}", timeout=5)
+    # Request data from openweather.com
+    open_weather_data = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + f"{user_input}&units=metric&APPID="
+        + f"{config['API']['key']}",
+        timeout=5,
+    )
+
+    # Error handling for data request, no city found / print data
+    if open_weather_data.json()["cod"] == "404":
+        print("No City Found")
+    else:
+        current_weather = open_weather_data.json()["weather"][0]["main"]
+        temp = round(open_weather_data.json()["main"]["temp"])
+
+        print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
+        print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
 
 
-# Error handling for data request, no city found / print data
-if open_weather_data.json()['cod'] == '404':
-    print("No City Found")
-else:
-    current_weather = open_weather_data.json()['weather'][0]['main']
-    temp = round(open_weather_data.json()['main']['temp'])
+elif choice == "2":
+    user_input = input("Enter your city: ")
+    print(user_input)
 
-    print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
-    print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
+    # Request data from openweather.com
+    open_weather_data = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + f"{user_input}&units=metric&APPID="
+        + f"{config['API']['key']}",
+        timeout=5,
+    )
+
+    # Error handling for data request, no city found / print data
+    if open_weather_data.json()["cod"] == "404":
+        print("No City Found")
+    else:
+        current_weather = open_weather_data.json()["weather"][0]["main"]
+        temp = round(open_weather_data.json()["main"]["temp"])
+
+        print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
+        print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
+
+elif choice == "3":
+      break
+    else:
+      print("Invalid option. Please Try Again.")
+
