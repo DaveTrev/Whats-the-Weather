2a33f001c0819bf8742d7bac5eea976140ec4a4b
DaveTrev <trevaskisdavid@gmail.com>
2023-04-16 06:32:03 +0000

    added humidity print out for current weather, added forecast data to readme

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ while True:
         print(user_input)
 
         # Request data from openweather.com
-        open_weather_data = requests.get(
+        open_weather_data_c = requests.get(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + f"{user_input}&units=metric&APPID="
             + f"{config['API']['key']}",
@@ -50,24 +50,33 @@ while True:
         )
 
         # Error handling for data request, no city found / print data
-        if open_weather_data.json()["cod"] == "404":
+        if open_weather_data_c.json()["cod"] == "404":
             print("No City Found")
         else:
-            current_weather = open_weather_data.json()["weather"][0]["main"]
-            temp = round(open_weather_data.json()["main"]["temp"])
+            current_weather = open_weather_data_c.json()["weather"][0]["main"]
+            temp = round(open_weather_data_c.json()["main"]["temp"])
+            humid = open_weather_data_c.json()["main"]["humidity"]
 
             print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
             print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
+            print(Fore.BLUE + f"The humidity in {user_input} is: {humid}%")
 
     elif choice == "2":
         user_input = input("Enter your city: ")
         print(user_input)
 
         # Request data from openweather.com
-        open_weather_data = requests.get(
-            " https://api.openweathermap.org/data/2.5/forecast?q={user_input}&units=metric&APPID={config['API']['key']}", timeout=5,
+        open_weather_data_f = requests.get(
+            " https://api.openweathermap.org/data/2.5/forecast?q="
+            + f"{user_input}&units=metric&APPID="
+            + f"{config['API']['key']}", timeout=5,
         )
+        print(open_weather_data_f.content)
 
+
+
+
+        """
         # Error handling for data request, no city found / print data
         if open_weather_data.json()["cod"] == "404":
             print("No City Found")
@@ -77,9 +86,8 @@ while True:
 
             print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
             print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
-
+        """
     elif choice == "3":
         break
     else:
         print("Invalid option. Please Try Again.")
-
