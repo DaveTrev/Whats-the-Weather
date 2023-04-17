c08d1c4933dece4fd58531dc9ed14b68edd9451a
DaveTrev <trevaskisdavid@gmail.com>
2023-04-17 16:51:00 +0000

    added fore.RED to invaild options

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 # Import libraries
 import os
 import configparser
+import sys
 import requests
 import pyfiglet
-import sys
 from colorama import Fore
 
 
 # Use configparser to read config.ini file
 if 'API_KEY' in os.environ:
-    config = {"API": {"key": None }}
+    config = {"API": {"key": None}}
     config["API"]["key"] = os.environ.get("API_KEY")
 else:
     config = configparser.ConfigParser()
@@ -45,24 +45,23 @@ while True:
         # Error handling for data request, no city found / print data
         if open_weather_data_c.json()["cod"] == "404":
             print("No City Found")
-        else:
+        else: # gets current weather data
             current_weather = open_weather_data_c.json()["weather"][0]["main"]
             temp = round(open_weather_data_c.json()["main"]["temp"])
             feels_like = open_weather_data_c.json()["main"]["feels_like"]
             humid = open_weather_data_c.json()["main"]["humidity"]
             clouds = open_weather_data_c.json()["clouds"]["all"]
 
-
             print(Fore.BLUE + f"The weather in {user_input} is: {current_weather}")
             print(Fore.BLUE + f"The temperature in {user_input} is: {temp}°C")
             print(Fore.BLUE + f"It feels like in {user_input} is: {feels_like}°C")
             print(Fore.BLUE + f"The humidity in {user_input} is: {humid}%")
             print(Fore.BLUE + f"The cloud cover in {user_input} is: {clouds}")
-            if 'Rain' in current_weather:
+            if 'Rain' in current_weather: # checks if weather data contains rain
                 print(Fore.YELLOW + "bring a jacket & a umbrella, it's going to rain today")
             else:
                 print(Fore.YELLOW + "Looks like no rain is due for today!")
-                
+
             while True:
                 print(Fore.GREEN + "Select what you would like to do next")
                 print(Fore.GREEN + "1) Check weather in another city.")
@@ -76,8 +75,7 @@ while True:
                 elif choice == "2":
                     sys.exit()  # give user option to quit or run again
                 else:
-                    print("Invalid option, please try again.")
-                
+                    print(Fore.RED + "Invalid option, please try again.")
 
     elif choice == "2":
         user_input = input("Enter your city, for a 5-day forecast:\n ")
@@ -91,7 +89,7 @@ while True:
         )
         if open_weather_data_f.json()["cod"] == "404":  # Error handling 4 data
             print("No City Found")
-        else:
+        else: # gets weather data for the next five days
             forecast_data_5 = open_weather_data_f.json()["list"]
             print(Fore.BLUE + f"5-day forecast for {user_input}: ")
             for data in forecast_data_5:
@@ -103,7 +101,7 @@ while True:
             while True:
                 print(Fore.GREEN + "Select what you would like to do next")
                 print(Fore.GREEN + "1) Check weather in another city.")
-                print(Fore.GREEN + "2) To return to the main menu.")
+                print(Fore.GREEN + "2) Quit the program.")
                 choice = input(Fore.GREEN + "Enter your choice: 1 or 2:\n ")
                 # removes or truncates the given characters from the beginning and the end of the original string
                 choice = choice.strip()        # change this menu option to return to main menu or quit program
@@ -113,9 +111,9 @@ while True:
                 elif choice == "2":
                     sys.exit()  # give user option to quit or run again
                 else:
-                    print("Invalid option, please try again.")
+                    print(Fore.RED + "Invalid option, please try again.")
 
     elif choice == "3":
         break
     else:
-        print("Invalid option. Please Try Again.")
+        print(Fore.RED + "Invalid option. Please Try Again.")
